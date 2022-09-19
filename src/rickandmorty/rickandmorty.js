43b2e117415ee8de3877d1932e8ac9fd8b46be1d
Viktor Ostafiichuk <ostafiyshyk@gmail.com>
2022-09-19 20:08:42 +0300

    add HW1-part2

---
+++ src/rickandmorty/rickandmorty.js
@@ -0,0 +1,15 @@
+export default function Rickandmorty (props) {
+    let {item} = props;
+
+    return (
+        <div>
+            <h1>{item.id} -- {item.name}</h1>
+            <ol>
+                <li>{item.status}</li>
+                <li>{item.species}</li>
+                <li>{item['gender']}</li>
+            </ol>
+            <img src={item.image} alt={item.name}/>
+        </div>
+    );
+}
---
+++ src/rickandmorty/rickandmortys.js
@@ -0,0 +1,20 @@
+import Rickandmorty from "./rickandmorty";
+import {useState} from "react";
+
+export default function Rickandmortys () {
+    let [rickandmortys, setRickandmortys] = useState([]);
+
+    fetch('https://rickandmortyapi.com/api/character')
+        .then(value => value.json())
+        .then(value => {
+            setRickandmortys(value.results);
+        })
+
+
+
+    return (
+        <div>
+            {rickandmortys.map((rickandmortys, index) => <Rickandmorty key={index} item={rickandmortys}/>)}
+        </div>
+    );
+}
